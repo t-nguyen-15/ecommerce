aa90b2af4517c72d4bd8706f1630ba17c884e633
Tony D Nguyen <tnguyen15@live.maryville.edu>
2022-07-24 15:27:27 -0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,25 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ecommerce"
+  end
+
+  test "should get root" do 
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get signin" do
+    get static_pages_signin_url
+    assert_response :success
+    assert_select "title", "Sign In | #{@base_title}"
   end
 end
