dedc331d4946f3583127bc7e7db3fa7ebdd1c2f4
Tony D Nguyen <tnguyen15@live.maryville.edu>
2022-08-14 17:01:17 -0700

    update email production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'tnguyen15@live.maryville.edu'
+  default from: 'tonydng123@gmail.com'
   layout 'mailer'
 end
 
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,7 +7,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["tnguyen15@live.maryville.edu"], mail.from
+    assert_equal ["tonydng123@gmail.com"], mail.from
     assert_match user.reset_token,        mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
