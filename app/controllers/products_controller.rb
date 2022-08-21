9c3445a58b574c2e1c4f675e3c359d112f0af32c
Tony D Nguyen <tnguyen15@live.maryville.edu>
2022-08-21 16:23:10 -0700

    add shopping cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,27 @@
 class ProductsController < ApplicationController
     before_action :logged_in_user, only:[:new, :create, :destroy]
     before_action :correct_user,   only: :destroy
+    before_action :initialize_session
+    before_action :load_cart
+    
     def index
         @products = Product.paginate(page: params[:page], per_page: 3)
+    end
+
+    def add_to_cart
+      id = params[:id].to_i
+      session[:cart] << id unless session[:cart].include?(id)
+      redirect_to root_path
+    end
+
+    def load_cart
+      @cart = Product.find(session[:cart])
+    end
+
+    def remove_from_cart
+      id = params[:id].to_i
+      session[:cart].delete(id)
+      redirect_to root_path
     end
 
     def new 
@@ -55,5 +74,9 @@ class ProductsController < ApplicationController
         def correct_user
             @product = current_user.products.find_by(id: params[:id])
             redirect_to root_url, status: :see_other if @product.nil?
+        end
+
+        def initialize_session
+          session[:cart] ||= []
         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   default_url_options :host => "tony-ecommerce.herokuapp.com/"
   get 'password_resets/new'
   get 'password_resets/edit'
+
+  get 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
  
   root              to: 'products#index'
   get '/signup',    to: 'users#new'
